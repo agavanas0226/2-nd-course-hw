e3ddea6317d1da9a3d796393ab7fb23a9f853bdc
agavanas0226 <agavanas0226@gmail.com>
2023-11-09 16:44:54 +0600

    первичная работа над hw15

--- buttonKom.js
+++ buttonKom.js
@@ -1,118 +1,21 @@
+import { fetchArray, arrayPost } from "./api.js"
+import { renderComments } from "./render.js";
 const nameElement = document.getElementById("inputName");
 const textElement = document.getElementById("inputText");
 const buttonElement = document.getElementById("buttonPush");
-const ulElement = document.getElementById("ul");
-const text = document.getElementById("comment-text");
-const editButton = document.getElementById("edit-button");
-const formHide = document.getElementById("form-add");
-let commentsArray = [
+
+
+export let commentsArray = [
 
 ];
-
-const fetchArray = () => {
-
-    fetch("https://wedev-api.sky.pro/api/v1/:kolesnikova-anastasiia/comments", {
-
-        method: "GET",
-
-    }
-    ).then((response) => {
-        if (response.status === 500) {
-            throw new Error("Ошибка сервера")
-        };
-        return response.json();
-    }).then((responseData) => {
-        commentsArray = [];
-        responseData.comments.map((element) => {
-            const newDate = new Date(element.date);
-            const elementObj = {
-                comment: element.text,
-                name: element.author.name,
-                like: element.likes,
-                userLike: element.isLiked,
-                date: newDate.toLocaleString()
-
-            }
-            commentsArray.push(elementObj);
-
-        }
-
-
-        )
-        document.getElementById("loadingFeed").style.display = 'none';
-        
-        renderComments();
-    }).catch((Error) => {
-        if (Error.message === 'Failed to fetch') {
-            alert("Проблемы с интернетом");
-        } else {
-            alert(Error.message)
-        }
-    })
-};
 fetchArray();
 
 
-const arrayPost = () => {
-    document.getElementById("form-add").style.display = 'none';
-    document.getElementById("loadingMessage").style.display = 'block';
-
-
-
-
-    fetch("https://wedev-api.sky.pro/api/v1/:kolesnikova-anastasiia/comments", {
-        method: "POST",
-        body: JSON.stringify({
-            text: textElement.value
-                .replaceAll("<", "&lt;")
-                .replaceAll(">", "&gt;"),
-            name: nameElement.value
-                .replaceAll("<", "&lt;")
-                .replaceAll(">", "&gt;"),
-                forceError: true,
-        })
-
-
-
-    }).then((thenresponse) => {
-        if (thenresponse.status === 500) {
-            arrayPost();
-            // throw new Error("Ошибка сервера");
-        } else if (thenresponse.status === 400) {
-            throw new Error("Имя или текст комментария должны иметь 3 и более символов");
-        }
-        if (thenresponse.ok) {
-
-
-            nameElement.value = '';
-            textElement.value = '';
-            buttonElement.disabled = true;
-        }
-        fetchArray();
-    }).catch((cathError) => {
-        if (cathError.message === 'Failed to fetch') {
-            alert("Проблемы с интернетом");
-        } else {
-
-
-            alert(`${cathError.message}`)
-        }
-    })
-        .finally(() => {
-
-
-            setTimeout(function () {
-                document.getElementById("form-add").style.display = 'flex';
-                document.getElementById("loadingMessage").style.display = 'none';
-
-            }, 1000);
-
-        });
-
+export const setComments = (comments) => {
+    commentsArray = comments;
 };
-
-const initEdit = () => {
-    let edit = document.querySelectorAll('.edit-button');
+export const initEdit = () => {
+        let edit = document.querySelectorAll('.edit-button');
     let textComment = document.querySelectorAll('.comment-text');
     for (const editButoon of edit) {
         const index = editButoon.dataset.edit;
@@ -131,8 +34,7 @@ const initEdit = () => {
     
     
                 commentsArray[index].isEdit = true;
-                renderComments();
-                
+                renderComments(commentsArray);                
             })
         } else {
                 editButoon.addEventListener('click', (event) => {
@@ -144,13 +46,13 @@ const initEdit = () => {
                 const value = document.querySelector('input').value;
                 commentsArray[index].comment = value;
                 commentsArray[index].isEdit = false;
-                renderComments();
+                renderComments(commentsArray);
             })
         }
     }
 }
 };
-const initDeleteButtonsListeners = () => {
+export const initDeleteButtonsListeners = () => {
     const deleteButtonsElements = document.querySelectorAll(".delete-button");
     for (const deleteButtonsElement of deleteButtonsElements) {
         deleteButtonsElement.addEventListener('click', (event) => {
@@ -159,7 +61,7 @@ const initDeleteButtonsListeners = () => {
             const index = deleteButtonsElement.dataset.index;
             console.log(index);
             commentsArray.splice(index, 1);
-            renderComments();
+            renderComments(commentsArray);
 
 
 
@@ -174,7 +76,7 @@ initDeleteButtonsListeners();
 
 
 
-const likes = () => {
+export const likes = () => {
     const likeButtons = document.querySelectorAll('.like-button');
     for (const likeButton of likeButtons) {
         likeButton.addEventListener('click', (event) => {
@@ -189,12 +91,12 @@ const likes = () => {
                 commentsArray[index].like -= 1;
                 commentsArray[index].userLike = false;
             }
-            renderComments();
+            renderComments(commentsArray);
         });
     };
 };
-const answer = () => {
-    const answerElement = document.querySelectorAll(".comment");
+export const answer = () => {
+    const answerElement = document.querySelectorAll(".answer-button");
     for (const answerElements of answerElement) {
         answerElements.addEventListener('click', (event) => {
             event.stopPropagation();
@@ -205,7 +107,7 @@ const answer = () => {
             addFormText.value = `${commentsArray[answerIndex].name} \n ${commentsArray[answerIndex].comment}`
 
 
-            renderComments();
+            renderComments(commentsArray);
 
 
 
@@ -221,47 +123,6 @@ answer();
 
 
 
-
-
-const renderComments = () => {
-    const commentsHtml = commentsArray.map((item, index) => {
-        return `
-        <li class="comment" data-answer="${index}">
-                <div  class="comment-header" >
-                  <div>${item.name}</div>
-                  <div>${item.date}</div>
-                </div>
-                <div class="comment-body">
-                ${!item.isEdit ? `<div data-comment='${index}' class="comment-text" >
-                ${item.comment}
-                
-                </div > ` : `<input value='${item.comment}'>`}                  
-                </div >
-    <div class="comment-footer">
-    
-        <button data-answer="${index}" class="answer-button">Ответить</button>
-        <button data-edit="${index}" class="edit-button">${!item.isEdit ? "Редактировать" : "Сохранить"}</button>
-        <button data-index='${index}' class="delete-button">Удалить</button>
-        
-        <div class="likes">
-            <span class="likes-counter">${item.like}</span>
-            <button data-index='${index}' class="like-button ${item.paint}"</button>
-    </div>
-                </div >
-                
-              </li >
-    `})
-          .join('');
-          ulElement.innerHTML = commentsHtml;
-          likes();
-          initDeleteButtonsListeners();
-          initEdit();
-      
-          answer();
-      
-      
-      };
-      renderComments();
 
 buttonElement.disabled = true;
 nameElement.addEventListener('input', () => {
@@ -319,7 +180,6 @@ textElement.addEventListener('input', () => {
     const dateString = `${currentDate.toLocaleDateString()} ${currentDate.toLocaleTimeString()} `;
 
     arrayPost();
-    renderComments();
 
     
 });
